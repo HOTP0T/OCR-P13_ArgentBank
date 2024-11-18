a96a0e749ee8d7043be467e8e4318209b41a00d6
HOTPOT <tpcmaky@gmail.com>
2024-11-18 14:56:53 +0100

    updated Header component to avoid duplicate with dashboardHeader

--- argent-bank-frontend/src/components/Header/Header.js
+++ argent-bank-frontend/src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
 import { FaUserCircle } from "react-icons/fa"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { signOut } from "../../features/featuresUser/actions/Action"
 
 import logo from '../../assets/argentBankLogo.png'
 
@@ -14,31 +15,59 @@ import '../Header/Header.css'
 
 const Header = () => {
 
-    const isLogged = useSelector(state => state.isLogged)
+  /**
+       * Get the user profile from the state
+       */
+  const user = useSelector(state => state.user)
 
-    return (
-        <header className="header">
-            <h1 className="srOnly">Argent Bank</h1>
-            <ul className="headerNav">
-                <Link className="logo" to="/" >
-                    <img src={logo} alt="Argent Bank logo" />
-                </Link>
-                <div className="signButton">
-                    <FaUserCircle className="userIcon" />
-                    {!isLogged && 
-                        <NavLink className={'navButton'} to='/SignIn'>
-                            Sign In
-                        </NavLink>
-                    }
-                    {isLogged && 
-                        <NavLink className={'navButton'} to='/'>
-                            Sign Out
-                        </NavLink>
-                    }
-                </div>                
-            </ul>
-        </header>
-    )
+  /**
+     * The hook that comes with React Router that will allow us to use the browser’s History API.
+     */
+  const nav = useNavigate()
+
+  /**
+   * The dispatch is used to send actions to the reducer
+   */
+  const dispatch = useDispatch()
+
+  /**
+   * The callback handleSubmit for the sign out button is triggered when the form is submitted.
+   * @param {*} event 
+   */
+  function handleLogout(event) {
+    event.preventDefault()
+    localStorage.removeItem('token')        // Token will be removed when sign out button is clicked
+    dispatch(signOut())
+    nav('/')
+  }
+
+  return (
+    <header className="header">
+      <h1 className="srOnly">Argent Bank</h1>
+      <ul className="headerNav">
+        <Link className="logo" to="/" >
+          <img src={logo} alt="Argent Bank logo" />
+        </Link>
+        <div className="signButton">
+
+          {!user.firstName &&
+            <NavLink className={'navButton'} to='/SignIn'>
+              Sign In
+            </NavLink>
+          }
+          {user.firstName &&
+            <>
+              <FaUserCircle className="userIcon" />
+              <p>{user.firstName}</p>
+              <NavLink className={'navButton'} onClick={handleLogout} to='#'>
+                Sign Out
+              </NavLink>
+            </>
+          }
+        </div>
+      </ul>
+    </header>
+  )
 }
 
 export default Header
